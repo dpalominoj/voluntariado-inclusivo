3a20a1eebdfd8f9f9f8a96d98b6d95e483000aec
dpalominoj <73992058@continental.edu.pe>
2025-04-25 22:50:35 -0500

    Create app.py

---
+++ phyton/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "¡Hola, mundo!"
+
+@app.route('/api/saludo/<nombre>')
+def saludo(nombre):
+    return jsonify({"mensaje": f"Hola, {nombre}!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
