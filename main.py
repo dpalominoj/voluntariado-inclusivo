ca7801ecc159605b909992c39d19183a051b97c4
dpalominoj <73992058@continental.edu.pe>
2025-05-04 12:06:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 def inicio():
     return render_template("inicio.html")
+
+@app.route('/actividades')
+def actividades():
+    return render_template('actividades.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/ayuda')
+def ayuda():
+    return render_template('ayuda.html')
     
 if __name__ == "__main__":
     app.run(debug=True)
